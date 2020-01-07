251c5a5d111b0c5ab70ca6f9ad6ec5455bec5b79
evvolution <zhouxiaoemail@163.com>
2020-01-07 22:03:12 +0800

    add new

--- js/control.js
+++ js/control.js
@@ -1,11 +1,14 @@
 $(document).ready(function(){
-
+    getlistvideo();
+    getlistmovie();
+    getlistcartoon();
 })
 
 window.onload = function() {
 	initSlide();
 }
 
+var link = "http://server.foshanplus.com/";
 
 function initSlide(){
     var tabsSwiper = new Swiper('.tabs-container', {
@@ -22,5 +25,137 @@ function initSlide(){
         $(".tabs .active").removeClass('active')
         $(this).addClass('active')
         tabsSwiper.slideTo($(this).index())
+    })
+}
+
+function getlistvideo(){
+    $.ajax({
+        type:"get",
+        url: link + "/exam/get_vote/?exam_id=15",
+        dataType:"json",
+        success:function(data){
+            console.log(data)
+            console.log(data.projects[0][0])
+
+            var line = "";
+            var modal = "";
+
+            for(var i=0;i<data.projects[0].length;i++){
+                var linetmp = '';
+                var line1 = '<li class="list-group-item" data-toggle="modal" data-target="#modalforvideo' + i + '">';
+                var line2 = '<div class="itempic"><img src="' + data.projects[0][i].pic_url + '"/><img src="img/play.png" class="fakeplay"/></div>';
+                var line3 = '<div class="iteminfo"><div>编号：<span>' + data.projects[0][i].item_id + '</span></div>';
+                var line4 = '<div>作品名称：<span>' + data.projects[0][i].title + '</span></div>';
+                var line5 = '<div>报送单位：<span>' + data.projects[0][i].content.split("报送单位:")[1].split("作品简介:")[0] + '</span></div></div>';
+                var line6 = '<div class="itemvotecount">' + data.projects[0][i].vote_count + '&nbsp;<img src="img/thumb.png" /></div></li>';
+                linetmp = line1 + line2 + line3 + line4 + line5 + line6;
+                line += linetmp;
+
+                var modaltmp = '';
+                var modal1 = '<div class="modal fade" id="modalforvideo' + i + '" tabindex="-1" role="dialog" aria-hidden="true"><div class="modal-dialog" role="document">';
+                var modal2 = '<div class="modalmain modal-content"><div class="modaltitlepic"><img src="img/modaltitle.png"></div>';
+                var modal3 = '<div class="modalcontent"><p class="modaldetailintro">标题：<span>' + data.projects[0][i].title + '</span></p><br/>';
+                var modal4 = '<p class="modaldetailintro">报送单位：<span>' + data.projects[0][i].content.split("报送单位:")[1].split("作品简介:")[0] + '</span></p>';
+                var modal5 = '<video class="fsvideo" controls="controls" poster="' + data.projects[0][i].pic_url + '" preload="none" src="' + data.projects[0][i].url + '"></video>';
+                var modal6 = '<div class="modaldetail">' + data.projects[0][i].content.split("作品简介:")[1] + '</div>';
+                var modal7 = '<button class="vote" onclick>投票</button></div></div></div></div>';
+                modaltmp = modal1 + modal2 + modal3 + modal4 + modal5 + modal6 + modal7;
+                modal += modaltmp;
+            }
+            $("#microvideo").html(line);
+            $("#modalsmicrovideo").html(modal)
+        },
+        error: function(){
+            console.log('getlistvideofailed');
+            alert("当前投票人数过多，请稍后重试");
+        }
+    })
+}
+
+function getlistmovie(){
+    $.ajax({
+        type:"get",
+        url: link + "/exam/get_vote/?exam_id=14",
+        dataType:"json",
+        success:function(data){
+            console.log(data)
+            console.log(data.projects[0][0])
+
+            var line = "";
+            var modal = "";
+
+            for(var i=0;i<data.projects[0].length;i++){
+                var linetmp = '';
+                var line1 = '<li class="list-group-item" data-toggle="modal" data-target="#modalformovie' + i + '">';
+                var line2 = '<div class="itempic"><img src="' + data.projects[0][i].pic_url + '"/><img src="img/play.png" class="fakeplay"/></div>';
+                var line3 = '<div class="iteminfo"><div>编号：<span>' + data.projects[0][i].item_id + '</span></div>';
+                var line4 = '<div>作品名称：<span>' + data.projects[0][i].title + '</span></div>';
+                var line5 = '<div>报送单位：<span>' + data.projects[0][i].content.split("报送单位:")[1].split("作品简介:")[0] + '</span></div></div>';
+                var line6 = '<div class="itemvotecount">' + data.projects[0][i].vote_count + '&nbsp;<img src="img/thumb.png" /></div></li>';
+                linetmp = line1 + line2 + line3 + line4 + line5 + line6;
+                line += linetmp;
+
+                var modaltmp = '';
+                var modal1 = '<div class="modal fade" id="modalformovie' + i + '" tabindex="-1" role="dialog" aria-hidden="true"><div class="modal-dialog" role="document">';
+                var modal2 = '<div class="modalmain modal-content"><div class="modaltitlepic"><img src="img/modaltitle.png"></div>';
+                var modal3 = '<div class="modalcontent"><p class="modaldetailintro">标题：<span>' + data.projects[0][i].title + '</span></p><br/>';
+                var modal4 = '<p class="modaldetailintro">报送单位：<span>' + data.projects[0][i].content.split("报送单位:")[1].split("作品简介:")[0] + '</span></p>';
+                var modal5 = '<video class="fsvideo" controls="controls" poster="' + data.projects[0][i].pic_url + '" preload="none" src="' + data.projects[0][i].url + '"></video>';
+                var modal6 = '<div class="modaldetail">' + data.projects[0][i].content.split("作品简介:")[1] + '</div>';
+                var modal7 = '<button class="vote" onclick>投票</button></div></div></div></div>';
+                modaltmp = modal1 + modal2 + modal3 + modal4 + modal5 + modal6 + modal7;
+                modal += modaltmp;
+            }
+            $("#micromovie").html(line);
+            $("#modalsmicromovie").html(modal)
+        },
+        error: function(){
+            console.log('getlistmoviefailed');
+            alert("当前投票人数过多，请稍后重试");
+        }
+    })
+}
+
+function getlistcartoon(){
+    $.ajax({
+        type:"get",
+        url: link + "/exam/get_vote/?exam_id=13",
+        dataType:"json",
+        success:function(data){
+            console.log(data)
+            console.log(data.projects[0][0])
+
+            var line = "";
+            var modal = "";
+
+            for(var i=0;i<data.projects[0].length;i++){
+                var linetmp = '';
+                var line1 = '<li class="list-group-item" data-toggle="modal" data-target="#modalforcartoon' + i + '">';
+                var line2 = '<div class="itempic"><img src="' + data.projects[0][i].pic_url + '"/><img src="img/play.png" class="fakeplay"/></div>';
+                var line3 = '<div class="iteminfo"><div>编号：<span>' + data.projects[0][i].item_id + '</span></div>';
+                var line4 = '<div>作品名称：<span>' + data.projects[0][i].title + '</span></div>';
+                var line5 = '<div>报送单位：<span>' + data.projects[0][i].content.split("报送单位:")[1].split("作品简介:")[0] + '</span></div></div>';
+                var line6 = '<div class="itemvotecount">' + data.projects[0][i].vote_count + '&nbsp;<img src="img/thumb.png" /></div></li>';
+                linetmp = line1 + line2 + line3 + line4 + line5 + line6;
+                line += linetmp;
+
+                var modaltmp = '';
+                var modal1 = '<div class="modal fade" id="modalforcartoon' + i + '" tabindex="-1" role="dialog" aria-hidden="true"><div class="modal-dialog" role="document">';
+                var modal2 = '<div class="modalmain modal-content"><div class="modaltitlepic"><img src="img/modaltitle.png"></div>';
+                var modal3 = '<div class="modalcontent"><p class="modaldetailintro">标题：<span>' + data.projects[0][i].title + '</span></p><br/>';
+                var modal4 = '<p class="modaldetailintro">报送单位：<span>' + data.projects[0][i].content.split("报送单位:")[1].split("作品简介:")[0] + '</span></p>';
+                var modal5 = '<video class="fsvideo" controls="controls" poster="' + data.projects[0][i].pic_url + '" preload="none" src="' + data.projects[0][i].url + '"></video>';
+                var modal6 = '<div class="modaldetail">' + data.projects[0][i].content.split("作品简介:")[1] + '</div>';
+                var modal7 = '<button class="vote" onclick>投票</button></div></div></div></div>';
+                modaltmp = modal1 + modal2 + modal3 + modal4 + modal5 + modal6 + modal7;
+                modal += modaltmp;
+            }
+            $("#microcartoon").html(line);
+            $("#modalsmicrocartoon").html(modal)
+        },
+        error: function(){
+            console.log('getlistcartoonfailed');
+            alert("当前投票人数过多，请稍后重试");
+        }
     })
 }
